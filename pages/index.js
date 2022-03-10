0fde973c1dd88cc0b329bbaf39db2757627a080d
droman892 <droman892@gmail.com>
2022-03-10 18:40:28 -0500

    updated

--- pages/index.js
+++ pages/index.js
@@ -3,37 +3,14 @@ import Image from 'next/image';
 import FeedbackForm from '../components/FeedbackForm';
 import styles from '../styles/Home.module.css';
 import Table from '@mui/material/Table';
+import StandardPage from '../layout/StandardPage';
+import FormContainer from '../layout/FormContainer';
 
 const Home = () => {
   return (
-    <div 
-      id='homeContainer' 
-      className='
-        flex 
-        justify-center 
-        items-center  
-        px-5
-        py-16
-        bg-gradient-radial
-        from-yellow-50
-        to-yellow-500'
-    >
-      <div className='
-      
-    
-        max-h-full
-        max-w-[100%]
-        p-5
-        bg-glassy 
-        rounded-[16px]
-        border-solid
-        border-ice
-        border-[1px]
-        shadow-glass
-        backdrop-blur-[5px]
-        md:max-w-[50%]
+    <StandardPage>
+      <FormContainer>
 
-      '>
         <h2 className='flex justify-center mb-4 text-xl font-bold'>
           Customer Feedback Form
         </h2>
@@ -47,8 +24,9 @@ const Home = () => {
           for many years to come.
         </h3>
         <FeedbackForm />
-      </div>
-    </div>
+
+      </FormContainer>
+    </StandardPage>
   )
 }
 
